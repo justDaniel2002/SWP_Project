1bee3b38f4938cdacd8ba1b2e487fc407ad1f4b7
hoang262 <hoangcanada2002@gmail.com>
2022-09-26 21:43:08 +0700

    comfig 2

--- Client/HappyProgramming/src/App.tsx
+++ Client/HappyProgramming/src/App.tsx
@@ -1,14 +1,23 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-
+import HomePage from './Page/HomePage'
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/'/> {/* home page */}
-        <Route path='/signIN'/> {/* sign in page */}
+        <Route path='/' element = {<HomePage/>}/> {/* home page */}
+        <Route path='/signIN'>
+          <Route path='/resetPw'/>{/* lay lai mk neu quen  */}
+        </Route> {/* sign in page */}
         <Route path='/signUp'/> {/* sign up page */}
-        <Route path='/'/>
+        <Route path="userProfile"/>
+        <Route path='/about'/>{/* about page show up general infomatio nabout web */}
+        <Route path='contact'/>{/* page to contact with admin */}
+        <Route path='/coursePage'/> {/* page show up list of course */}
+        <Route path='/admin'>
+          <Route path='/userControl'/>
+          <Route path='/courseControl'/>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
